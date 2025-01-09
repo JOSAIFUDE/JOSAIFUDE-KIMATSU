5e91baaceaf28c9b2eea148449714d3ee3388085
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-09 15:23:38 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,7 +1,7 @@
 function hyoji() {
 var str = "私はプリンです！";
 var cnt = document.timer.moji.value.length;
-if ( cnt < 9 ) {
+if ( cnt < 8 ) {
 document.timer.moji.value = str.substr(0,cnt+1); } else {
 document.timer.moji.value = ""; }
-setTimeout("hyoji()",700); }
+setTimeout("hyoji()",500); }
