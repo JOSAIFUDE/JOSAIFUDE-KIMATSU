c7cc8ddc61bcd2afaa6413d7594d15ff5846c93b
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-09 14:28:13 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,17 +1,7 @@
-function hyoji13() { 
-  //表示する文字
-  var str13 = "一私はプリンです。"; 
-  
-  //テキストボックスの文字数
-  var cnt13 = document.timer13.moji13.value.length; 
-  
-  //文字が全部表示されているか確認
-  if ( cnt13 < 11 ) { 
-    //現在より1文字多く切り出して表示
-    document.timer13.moji13.value = str13.substr(0,cnt13+1); } 
-  else { 
-    //全て表示されたら、空文字に戻す
-    document.timer13.moji13.value = ""; } 
-  
-  //setTimeout()を含む関数を呼び出す
-  setTimeout("hyoji13()",1000); }
+function hyoji() {
+var str = "私はプリンです！";
+var cnt = document.timer.moji.value.length;
+if ( cnt <　11 ) {
+document.timer.moji.value = str.substr(0,cnt+1); } else {
+document.timer.moji.value = ""; }
+setTimeout("hyoji()",800); }
