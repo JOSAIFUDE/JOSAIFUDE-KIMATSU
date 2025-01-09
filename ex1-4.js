4556cf7017de2a56c81fedb0f411db28302021a5
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-09 17:52:07 +0900

    Update ex1-4.js

--- ex1-4.js
+++ ex1-4.js
@@ -1,2 +1,2 @@
-function imgchange2(){document.getElementById('logo2').src='./image/bulboff.gif';}
-function imgmotosu2(){document.getElementById('logo2').src='./image/bulbon.gif';}
+function imgchange2(){document.getElementById('logo2').src='./image/purin.png';}
+function imgmotosu2(){document.getElementById('logo2').src='./image/purin1.png';}
