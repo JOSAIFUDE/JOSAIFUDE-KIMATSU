5d724abf6c43bf2ea3360ef4b64fc8720d5b3c4f
JOSAIFUDE <167833161+JOSAIFUDE@users.noreply.github.com>
2025-01-09 15:21:10 +0900

    Update ex1-3.js

--- ex1-3.js
+++ ex1-3.js
@@ -1,2 +1,2 @@
-function imgchange1() {document.getElementById('logo1').src= './image/purin.png'; setTimeout('imgback3()',3000);}
+function imgchange1() {document.getElementById('logo1').src= './image/purin.png'; setTimeout('imgback1()',3000);}
 function imgback1() {document.getElementById('logo1').src='./image/purin1.png';}
